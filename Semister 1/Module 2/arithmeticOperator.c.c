1c232d338aaba646c5b7a958684630d7d83dc5dc
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 08:32:22 -0800

    addition operator

---
+++ Semister 1/Module 2/arithmeticOperator.c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, addition;
+    num1 = 20;
+    num2 = 50;
+    addition = num1 + num2;
+    printf("Addition Result %d", addition);
+    return 0;
+}
--- Semister 1/Module 2/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main (){
-    int name , age ;
-    name = "Shakil";
-    age = 30 ;
-    printf("My Name Is %d\nIm %d Years Old", name , age );
-}
