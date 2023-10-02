fe8d9c04581c7467d62632b01ac3b43792a964f6
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 10:36:55 -0800

    conditonal statement

---
+++ Semister 1/Module 2/conditonalOperator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int taka;
+    scanf("%d", &taka);
+    if (taka == 100 || taka>100)
+    {
+        printf("Can i eiting burgur ");
+    }
+    else
+    {
+        printf("ami kichu khabo na ");
+    }
+    return 0;
+}
