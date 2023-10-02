5422723b79779ab3b3bcc98f3b463fe61a5ec386
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 08:41:30 -0800

    multiplication operator

--- Semister 1/Module 2/arithmeticOperator.c
+++ Semister 1/Module 2/arithmeticOperator.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int num1, num2, num3, addition, subtraction;
+    int num1, num2, num3, addition, subtraction, multiplication;
     num1 = 20;
     num2 = 50;
     num3 = 10;
     addition = num1 + num2; // additiom Operator
     subtraction = num1 - num3; // Subtraction Operator 
-    printf("Addition Result = %d\nSubtraction Result = %d", addition, subtraction);
+    multiplication = num1 * num3;
+    printf("Addition Result = %d\nSubtraction Result = %d\nMultipliction Result = %d", addition, subtraction, multiplication);
     return 0;
 }
