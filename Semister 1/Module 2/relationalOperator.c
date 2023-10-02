7757dd8f0b1e61dac027c081b4ab0079b9a123de
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 11:23:26 -0800

    logical operator

---
+++ Semister 1/Module 2/relationalOperator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+    /*
+    < = Less than 
+    > = grater than 
+    <= = less than equal 
+    >= = grater thean equal 
+    != = not equal 
+    == = equal 
+    ex : 10 > 2 here 10 is big from 2 
+
+    
+    */
+   
+}
