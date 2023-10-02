dfa703707f8d165b2a58a087d75ac6da56db7ec9
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 11:27:59 -0800

    logical operator

---
+++ Semister 1/Module 2/logicalOperator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+    /*
+    && ITS A LOGICAL OPERATOR 
+    T && T = T
+    T && F = F
+    F && T = F
+    F && F = F
+
+    || ITS A LOGICAL OPERATOR 
+    T || T= T
+    T || F = T
+    F || T = T
+    F || F = T
+    */
+}
