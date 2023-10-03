9de24457547302938fb2aa1979563525f5e85b04
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-03 07:46:11 -0800

    module 3 for loop

---
+++ Semister 1/Module 3/ForLoop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    // for (i = 1; i <= 5; i = i + 1)
+    // {
+    //     printf("Hello IM Soliman\n");
+    // }
+    for(i=1 ; i<= 5; i++){
+        printf("Hello IM \n");
+    }
+    return 0;
+}
