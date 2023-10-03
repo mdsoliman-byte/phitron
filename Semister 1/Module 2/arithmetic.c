0d9409cf86b3522d9e9d8dd33bf625f4b0cdc226
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 21:01:30 -0800

    module 2 complite

---
+++ Semister 1/Module 2/arithmetic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    // int x, y, divition;
+    // x = 5;
+    // y = 2;
+    // divition = x / y;
+    // printf("%d %d %d", x, y, divition);
+    int x = 0 ;
+    if (x=1)
+    {
+        printf("Hello ");
+    }else{
+        printf("no ");
+    }
+    
+    return 0;
+}
