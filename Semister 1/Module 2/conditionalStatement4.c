f90e913315f1899c120798c156109f887350c8b0
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-02 20:49:17 -0800

    conditonal statement 4

---
+++ Semister 1/Module 2/conditionalStatement4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    int taka ;
+    scanf("%d", &taka);
+    if (taka >= 5000)
+    {
+        printf("I want to travel cox bazar");
+
+    }
+    if (taka <= 5000)
+    {
+        printf("ami cox bazar jabo na ");
+    }
+    
+    return 0 ;
+    
+}
