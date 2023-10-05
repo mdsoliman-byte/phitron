d2115a64435b8a83422fd8a123ddfe3b7733e291
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-05 06:10:28 -0800

    while loop

--- Semister 1/Module 3/continueStatement.c
+++ Semister 1/Module 3/continueStatement.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i;
-    for (i = 1; i <= 10; i = i + 1)
+    for (i = 1; i <= 10; i = i + 1) // initilaigation , condition , increment 
     {
         if(i == 5 ){
             continue;
---
+++ Semister 1/Module 3/whileLoop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;
+    i = 1;
+    while (i <= 10) // condition
+    {
+        printf("%d\n ", i); // work
+        i = i + 1;          // increment / decrement
+    }
+    return 0;
+}
