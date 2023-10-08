5b218b832998e56dc5e32e13be0465cfa46dbf1d
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-08 04:44:51 -0800

    problem M complite

--- Semister 1/Module 4/Probem3Modify.c
+++ Semister 1/Module 4/Probem3Modify.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
+    /*
+    Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
+    */
     char a;
     scanf("%c", &a);
     if (a >= 'a' && a <= 'z')
