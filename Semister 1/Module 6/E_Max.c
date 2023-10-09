8140c3e3f5d6aea4ec33f67e74b188c452760780
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-08 23:53:26 -0800

    int min and int max value

--- Semister 1/Module 6/E_Max.c
+++ Semister 1/Module 6/E_Max.c
@@ -1,18 +1,32 @@
-    #include <stdio.h>
-    int main()
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    // int n;
+    // scanf("%d", &n);
+    // int a;
+    // int b = 0;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     scanf("%d", &a);
+    //     if (a > b)
+    //     {
+    //         b = a;
+    //     }
+    // }
+    // printf("%d", b);
+    int a, b;
+    scanf("%d", &a);
+    
+    int max = INT_MIN, min = INT_MAX;
+    for (int i = 1; i <= a; i++)
     {
-        int n;
-        scanf("%d", &n);
-        int a;
-        int b = 0;
-        for (int i = 1; i <= n; i++)
+        scanf("%d", &b);
+        if (b < min)
         {
-            scanf("%d", &a);
-            if (a > b)
-            {
-                b = a;
-            }
+            min = b;
         }
-        printf("%d", b);
-        return 0;
     }
+    printf("%d", b);
+    return 0;
+}
