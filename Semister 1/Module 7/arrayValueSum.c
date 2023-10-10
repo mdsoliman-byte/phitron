f64c0d68ebe194ab357e1fdb42294627fc782d23
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-10 08:11:19 -0800

    array main conseft done

--- Semister 1/Module 7/arrayValueSum.c
+++ Semister 1/Module 7/arrayValueSum.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int hello[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &hello[i]);
-    }
-    int sum = 0;
-    for (int i = 0; i < n; i++)
-    {
-        sum = sum + hello[i];
-    }
-    printf("%d", sum);
+    // int n;
+    // scanf("%d", &n);
+    // int hello[n];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &hello[i]);
+    // }
+    // int sum = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     sum = sum + hello[i];
+    // }
+    // printf("%d", sum);
 
+    int arr[] = {10, 20, 30};
+    printf("%d", arr[3]);
     return 0;
 }
