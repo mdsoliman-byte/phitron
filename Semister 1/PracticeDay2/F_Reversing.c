8e811efaa434aefb6e9b781893a214641e26fc4b
mdsoliman-byte <developer.mdsoliman@gmail.com>
2023-10-12 10:24:05 -0800

    reversing array

---
+++ Semister 1/PracticeDay2/F_Reversing.c
@@ -0,0 +1,22 @@
+/*
+Problem F : Reversing Array ;
+Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F
+*/ 
+
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int j = n - 1; j >= 0; j--) 
+    {
+        printf("%d ", arr[j]);
+    }
+
+    return 0;
+}
